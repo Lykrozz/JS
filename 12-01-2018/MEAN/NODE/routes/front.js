089da59332d4208e1af180b7f361b9735436708f
Lykrozz <gosset.jonathan@gmail.com>
2018-01-12 16:02:34 +0100

    Division html en sections

--- 12-01-2018/MEAN/NODE/routes/front.js
+++ 12-01-2018/MEAN/NODE/routes/front.js
@@ -2,14 +2,43 @@
 Importer les composants de la route
  */
     // Class
-    const express = require('express');
-    const router  = express.Router();
+    const express    = require('express');
+    const router     = express.Router();
+    const bodyParser = require('body-parser');
+
+    // Middleware
+    router.use(bodyParser.urlencoded({extended: true}));
+    router.use(bodyParser.json());
+
+/*
+Créer une collection de données
+ */
+    const indexCollection = [
+        {
+            title : 'Nodejs',
+            content : 'App server'
+        },
+        {
+            title : 'Expressjs',
+            content : 'App server'
+        },
+        {
+            title : 'MongoDB',
+            content : 'Base de données NOsql'
+        },
+        {
+            title : 'Angular',
+            content : 'One framework (to rule dem all)'
+        }
+    ];
+
+
 /*
 Définir la ou les routes
  */
     // Page home
     router.get( `/`, ( req, res ) => {
-        res.render( `index` );
+        res.render( `index`, { title : 'Titre de ta page' , content: indexCollection} );
     } );
 /*
 Exporter le module de la route
